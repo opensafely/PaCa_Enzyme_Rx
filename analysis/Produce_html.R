dcd24ea3248e52a28ec78b63e72ecbc574da7756
lemanska <a.lemanska@surrey.ac.uk>
2022-11-11 14:40:33 +0000

    remove figures
    temporary remove figures due to error

--- analysis/Produce_html.R
+++ analysis/Produce_html.R
@@ -5,5 +5,5 @@ library(here)
 library(MASS)
 library(rmarkdown)
 
-rmarkdown::render(here::here("analysis", "Report_test1.Rmd"), 
+rmarkdown::render(here::here("analysis", "Report_test2.Rmd"), 
                   output_file = "Test1.html", output_dir = "output")
