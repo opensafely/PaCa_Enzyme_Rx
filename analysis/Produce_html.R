8baf59fa2aa17b60727d128a9fa698a6f80d9bf2
lemanska <a.lemanska@surrey.ac.uk>
2023-10-12 21:50:13 +0100

    file name

--- analysis/Produce_html.R
+++ analysis/Produce_html.R
@@ -6,4 +6,5 @@ library(MASS)
 library(rmarkdown)
 
 rmarkdown::render(here::here("analysis", "Report_html.Rmd"), 
-                  output_file = "Report_file.html", output_dir = "output")
+                  output_file = "Report_file_Oct2023.html", 
+                  output_dir = "output")
